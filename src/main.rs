f44dfae4a5b44f17dc86edd308c3fd662bf1c3c5
coderasha <ashadipta@gmail.com>
2022-08-03 19:55:09 +0530

    bubble sort

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    let mut arr =["pupu","chanchi", "ani","gelhu", "buji", "goat"];
+    bubble_sort(&mut arr);
+    println!("Hello, world!{:?}",arr.len());
+}
+pub fn bubble_sort<T: Ord>(arr:&mut [T]){
+
+    for i in 0..arr.len(){
+
+        for j in 0..arr.len() -1 -i{
+            if arr[j] > arr[j+1]{
+                arr.swap(j,j+1);
+            }
+            
+        }
+    }
 }
