e17cdea418bd0c0a0c8348b96e3cf99d99df2c26
coderasha <ashadipta@gmail.com>
2022-08-04 00:54:53 +0530

    change

--- insertion_sort/src/main.rs
+++ insertion_sort/src/main.rs
@@ -1,25 +1,26 @@
-fn main() {
-    // let mut v = vec![8,6,7,3,9,2];
-    let mut v = vec!["pupu","chanchi","ani","koko","kash","ham"];
-insertion_sort(&mut v );
-    println!("{:?}",v);
+// fn main() {
+//     // let mut v = vec![8,6,7,3,9,2];
+//     let mut v = vec!["pupu","chanchi","ani","koko","kash","ham"];
+// insertion_sort(&mut v );
+//     println!("{:?}",v);
+// }
+// fn insertion_sort<T:PartialOrd>(arr:&mut Vec<T>){
+//     for i in 1..arr.len(){
+//         let mut j = i;
+//         while j>0 && arr[j]< arr[j-1]{
+
+//             arr.swap(j,j-1);
+//             j=j-1 ;
+//         }
+
+//     }
+
+// }
+
+fn main(){
+let mut v = vec![1,7,4,8,9,3];
+v.sort();
+print!("{:?\n} ",v);
+
 }
-fn insertion_sort<T:Ord>(arr:&mut Vec<T>){
-    for i in 1..arr.len(){
-        let mut j = i;
-        while j>0 && arr[j]< arr[j-1]{
 
-            arr.swap(j,j-1);
-            j=j-1 ;
-        }
-
-
-
-
-    }
-
-
-
-  
-
-}
